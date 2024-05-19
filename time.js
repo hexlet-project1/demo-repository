5fb592a9231f3e466e233f05cef95ae6ff40dac5
Saint <146729423+WhereIU@users.noreply.github.com>
2024-05-19 17:29:52 +0000

    update files

--- time.js
+++ time.js
@@ -4,6 +4,6 @@ function displayTime() {
     let element = document.getElementById("clock"); // Найти элемент с id="clock" 
     let now = new Date();    // Получить текущее время
     element.innerHTML = now.toLocaleTimeString(); //_.sum([4,5,6,7]); // Отобразить время
-    setTimeout(displayTime, 1000);    // Вызывать функцию каждую секунду
+    //setTimeout(displayTime, 1000);    // Вызывать функцию каждую секунду
 }
 window.onload = displayTime; // Начать отображение времени после загрузки документа
