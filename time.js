b62c582cece07814f6630d32dd11eac1f2da325d
Saint <146729423+WhereIU@users.noreply.github.com>
2024-05-20 04:47:40 +0300

    Update time.js

--- time.js
+++ time.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 function displayTime() {
     let element = document.getElementById("clock"); // Найти элемент с id="clock" 
     let now = new Date();    // Получить текущее время
-    element.innerHTML = now.toLocaleTimeString(); // Отобразить время
+    element.innerHTML = _.sum([5,6,7]);//now.toLocaleTimeString(); // Отобразить время
     //setTimeout(displayTime, 1000);    // Вызывать функцию каждую секунду
 }
 window.onload = displayTime; // Начать отображение времени после загрузки документа
